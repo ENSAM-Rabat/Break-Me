57c0222c11a932b08c0cfe119f3baca313a97664
Younes <26528746+Y0un3ss3@users.noreply.github.com>
2022-01-27 21:38:23 +0100

    Create Say_My_Name.c

---
+++ Say_My_Name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+double list[] = { 17234491258674.724609375, 6 };
+char *node = (char *)list;
+int main(){
+
+list[1] && (list[0] /= 2,  putchar(main(--list[1], (char **)++node)));
+return *--node;
+}
