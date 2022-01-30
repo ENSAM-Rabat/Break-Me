7a0e6b0e65b51d6960814932dbd34ae4bf581cdc
Hamza Ait Bourhim <69424447+hamzaaitbourhim@users.noreply.github.com>
2022-01-31 00:34:37 +0100

    Add files via upload

---
+++ solutions/Say_My_Name/hmza/reverse-engineered.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+double list[] = {2299151115.421189, 5};
+char *node = (char *)list;
+
+int main()
+{
+    list[1] && (list[0] /= 2, putchar(main(--list[1], (char **)++node)));
+    return *--node;
+}
