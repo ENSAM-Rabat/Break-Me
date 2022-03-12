fc66ba14e68e6a3c94444d4f894328b75ddb7174
ouihrani <101366987+ouihrani@users.noreply.github.com>
2022-03-12 22:49:12 +0100

    Create Say_My_Name.c
    ouihrani

---
+++ OssamaOuihrani/Say_My_Name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+double list[] = { 1670594901526.836182, 6 };
+char *node = (char *)list;
+int main(){
+
+list[1] && (list[0] /= 2,  putchar(main(--list[1], (char **)++node)));
+return *--node;
+}
