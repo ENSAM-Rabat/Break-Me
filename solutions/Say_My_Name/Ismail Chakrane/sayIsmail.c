fff9149486e3d9477a2e5a3f8b12a5220b506d81
ismailchakrane <61493635+ismailchakrane@users.noreply.github.com>
2022-03-16 19:20:31 +0100

    Add files via upload

---
+++ solutions/Say_My_Name/Ismail Chakrane/sayIsmail.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double list[] = {17200164810932.71, 6 };
+char *node = (char *)list;
+int main(){
+	
+list[1] && ( list[0] /= 2,putchar(main(--list[1], (char **)++node)));
+return *--node;
+}
